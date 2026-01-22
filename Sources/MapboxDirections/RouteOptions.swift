185a85583ee0b8979d3ab16177f718af22f19830
Dmitry Yashkin <dmitry.yashkin@toptal.com>
2026-01-22 09:00:13 +0300

    Fixes for Rever

--- Sources/MapboxDirections/RouteOptions.swift
+++ Sources/MapboxDirections/RouteOptions.swift
@@ -303,7 +303,7 @@ open class RouteOptions: DirectionsOptions, @unchecked Sendable {
         self.includesVisualInstructions = matchOptions.includesVisualInstructions
     }
 
-    override var abridgedPath: String {
+    override open var abridgedPath: String {
         return "directions/v5/\(profileIdentifier.rawValue)"
     }
 
